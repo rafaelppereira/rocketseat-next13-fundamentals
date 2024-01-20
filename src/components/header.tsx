b02bbcc93241498c8075d453b6fbaccdf6ce3862
Rafael Pereira <rafaelsantospereira03@gmail.com>
2024-01-20 02:39:49 -0300

    git

---
+++ src/components/header.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import { Search, ShoppingBag } from 'lucide-react'
+import Image from 'next/image'
+
+export function Header() {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <Link href="/" className="text-2xl font-extrabold text-white">
+          devstore
+        </Link>
+
+        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+          <Search className="w-5 h-5 text-zinc-500" />
+
+          <input
+            type="text"
+            placeholder="Buscar produtos..."
+            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+          />
+        </form>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <ShoppingBag className="w-4 h-4" />
+          <span className="text-sm">Cart (0)</span>
+        </div>
+
+        <div className="w-px h-4 bg-zinc-700" />
+
+        <Link href="/" className="flex items-center gap-2 hover:underline">
+          <span className="text-sm">Account</span>
+          <Image
+            alt=""
+            width={24}
+            height={24}
+            className="h-6 w-6 rounded-full"
+            src="https://github.com/rafaelppereira.png"
+          />
+        </Link>
+      </div>
+    </div>
+  )
+}
